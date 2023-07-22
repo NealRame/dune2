a89e63a45d947df83dba1ac9bacee2da46771576
Julien Graziano <julien@graziano.fr>
2023-07-23 00:30:17 +0200

    build: rc creation

---
+++ scripts/env.sh
@@ -0,0 +1,10 @@
+#! /usr/bin/env bash
+
+set -e
+set -o pipefail
+set -u
+
+export DUNE2_DATA_DIR="${DUNE2_DATA_DIR:-$PWD/data}"
+export DUNE2_RESOURCES_DIR="${DUNE2_DIR:-$PWD/resources}"
+
+[ -f "$PWD/.dune2.env" ] && source .dune2.env
