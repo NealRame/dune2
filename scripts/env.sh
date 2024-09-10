fa29f722d870acfd6fb215e961b59c821c387ffa
Julien Graziano <julien@graziano.fr>
2024-09-10 23:21:11 +0200

    build: update build scripts

--- scripts/env.sh
+++ scripts/env.sh
@@ -5,10 +5,9 @@ set -o pipefail
 set -u
 
 export DUNE2_DATA_DIR="${DUNE2_DATA_DIR:-$PWD/data}"
-export DUNE2_RESOURCES_DIR="${DUNE2_DIR:-$PWD/packages/resources}"
+export DUNE2_RESOURCES_DIR="${DUNE2_DIR:-$PWD/packages/dune2-resources}"
 
 if [ -f "$PWD/.env" ];
 then
     source "$PWD/.env"
 fi
-
