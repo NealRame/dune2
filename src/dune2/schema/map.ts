2f6de8c7a074754427e59bb5704dc915ec11b3de
Julien Graziano <julien@graziano.fr>
2024-11-17 00:56:17 +0100

    refactor: update map generator config schemas

--- src/dune2/schema/map.ts
+++ src/dune2/schema/map.ts
@@ -3,7 +3,7 @@ import { z } from "zod"
 export const DuneMapGeneratorSizeSchema = z.object({
     width: z.number(),
     height: z.number(),
-}).required()
+})
 export type TDuneMapGeneratorSizeConfig =
     z.infer<typeof DuneMapGeneratorSizeSchema>
 
@@ -13,7 +13,7 @@ export const DuneMapGeneratorTerrainSchema = z.object({
     sandThreshold: z.number().min(0).max(1),
     rockThreshold: z.number().min(0).max(1),
     mountainsThreshold: z.number().min(0).max(1),
-}).required()
+})
 export type TDuneMapGeneratorTerrainConfig =
     z.infer<typeof DuneMapGeneratorTerrainSchema>
 
@@ -31,6 +31,6 @@ export const DuneMapGeneratorConfigSchema = z.object({
     size: DuneMapGeneratorSizeSchema,
     terrain: DuneMapGeneratorTerrainSchema,
     spice: DuneMapGeneratorSpiceSchema,
-}).required()
+})
 export type TDuneMapGeneratorConfigConfig =
     z.infer<typeof DuneMapGeneratorConfigSchema>
